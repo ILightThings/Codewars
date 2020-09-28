16a56b2dfe502bb3baaf5731be1534a582a1339e
iLightThings <gitgub@ggray.info>
2020-09-28 09:01:04 -0400

    Update main.py

--- Python/English Begger/main.py
+++ Python/English Begger/main.py
@@ -1,10 +1,11 @@
 def beggars(values, n):
     sum = 0
-    for x in range(0,len(values)):
+
+    for x in range(0,):
         sum = sum + (values[x])
     return sum
         
     #your code here
 
 
-print(beggars([1,2,3,4,5],1))
+print(beggars([1,2,3,4,5],1))   
